da6ece0aa06f5e6ed89e526d1bbe988d3901af38
bruno <bruno@soundmap.io>
2022-09-14 15:43:30 +0200

    added missing add and remove listener functions

--- android/src/main/java/de/patwoz/rn/bluetoothstatemanager/RNBluetoothStateManagerModule.java
+++ android/src/main/java/de/patwoz/rn/bluetoothstatemanager/RNBluetoothStateManagerModule.java
@@ -115,6 +115,14 @@ public class RNBluetoothStateManagerModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
+  public void addListener(String eventName) {
+  }
+
+  @ReactMethod
+  public void removeListeners(Integer count) {
+  }
+
+  @ReactMethod
   public void disable(Promise promise) {
     if (this.handleIfBluetoothNotSupported(promise)) { return; }
     Activity currentActivity = this.handleCurrentActivity(promise);
